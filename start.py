6b9599641f20630b4104560cff62024fa10848a4
shrutisharma91 <www.7425shruti@gmail.com>
2025-04-24 17:56:59 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    # Start the FastAPI server
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
